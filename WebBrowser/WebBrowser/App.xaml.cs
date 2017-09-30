8cd2bc17f97ca465018655853cfc82cb3f617b5a
atinst <qav@qq.com>
2017-10-01 00:15:22 +0800

    WebBrowser

---
+++ WebBrowser/WebBrowser/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfApp1
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ WebBrowser/WebBrowser/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UrlIn_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter) return;
+            var word = UrlIn.Text;
+            var url = $"http://www.igimu.com/words/index.php?q={word}";
+            CamWeb.Navigate(url);
+        }
+
+        private void UrlIn_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // ReSharper disable once AssignNullToNotNullAttribute
+            UrlIn.Text = null;
+        }
+    }
+}
